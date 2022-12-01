3193a8317d9b0cfaaae0178ae0f8d0fe90306c98
yemredd <81518992+yemredd@users.noreply.github.com>
2022-12-01 22:18:46 +0300

    .
    .

---
+++ Assets/Scripts/planetkod.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetkod : MonoBehaviour
+{
+    public float scoreR;
+    public float scoreL;
+    public GameObject[] planet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void getscorer(){
+
+    }
+    public void getscorel(){
+        
+    }
+    public void createplanet(){
+
+    }
+}
