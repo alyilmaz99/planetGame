964d7eb2973572096c396600a5bcc013afb57322
yemredd <81518992+yemredd@users.noreply.github.com>
2022-12-12 20:18:53 +0300

    fbp
    fireball, blackhole, planetdirection

--- Assets/Scripts/Planets/RotatePlanets.cs
+++ Assets/Scripts/Planets/RotatePlanets.cs
@@ -9,9 +9,13 @@ public class RotatePlanets : MonoBehaviour
     [SerializeField] private float rotationSpeed;
     private Vector3 rotationVector = new Vector3(0, 0, 1);
     int speed;
+    int direction;
 
     void Start()
     {
+        PlayerPrefs.SetInt("planetno", PlayerPrefs.GetInt("planetno") + 1);
+        if(PlayerPrefs.GetInt("planetno") > 9)
+        direction = Random.Range(0,2);
     }
     void Update()
     {
@@ -19,6 +23,9 @@ public class RotatePlanets : MonoBehaviour
             speed = 1 +(PlayerPrefs.GetInt("score")/500);
         }
         speed = 1 +(PlayerPrefs.GetInt("score")/500);
+        if(direction == 0)
         transform.Rotate(rotationVector * speed *rotationSpeed * Time.deltaTime);
+        if(direction == 1)
+        transform.Rotate(rotationVector * -speed *rotationSpeed * Time.deltaTime);
     }
 }
