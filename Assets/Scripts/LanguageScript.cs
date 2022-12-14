0c89ddc99996388dd968af6ac88116b4b11de5d1
yemredd <81518992+yemredd@users.noreply.github.com>
2022-12-14 20:03:20 +0300

    .
    .

--- Assets/Scripts/LanguageScript.cs
+++ Assets/Scripts/LanguageScript.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LanguageScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Text> Scenetexts;
+    public List<SceneTranslater> translaters;
+
     void Start()
     {
+        Debug.Log(Scenetexts.Count);
+        
+        
         
     }
 
-    // Update is called once per frame
+    public void translate(int x){
+        for(int i = 0; i < translaters.Count; i++){
+            if(x == 0)
+            Scenetexts[i].text = translaters[i].English;
+            if(x == 1)
+            Scenetexts[i].text = translaters[i].Turkish;
+        }
+    }
+
+    
     void Update()
     {
-        
+        if(Application.systemLanguage == SystemLanguage.English)
+        translate(0);
+        if(Application.systemLanguage == SystemLanguage.Turkish)
+        translate(1);
+    }
+}
+ [System.Serializable]
+public class SceneTranslater{
+    public string Turkish;
+    public string English;
+
+    public SceneTranslater(string turkish, string english){
+        Turkish = turkish;
+        English = english;
     }
 }
